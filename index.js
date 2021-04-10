95978a397c32db1a5afef3f9e82a08fdac1be76a
FR33-KICK <80514181+FR33-KICK@users.noreply.github.com>
2021-04-10 14:29:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const client = new Discord.Client()
 
 
 client.on("ready", () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        bot.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 10000); // Runs this every 10 seconds.
     console.log(`Logged in as ${client.user.tag}`)
     const voiceChannel = client.channels.cache.get(channel_id)
     voiceChannel.join().then(connection => {
