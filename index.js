b52f08e3658d84d0c6c1ff4f67fc577de43394e4
FR33-KICK <80514181+FR33-KICK@users.noreply.github.com>
2021-07-01 16:30:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 const Discord = require("discord.js")
 const ytdl = require("ytdl-core")
+const config = require("./config.json");
 const {
     token,
     channel_id,
     video_urls
 } = require("./config.json")
-const client = new Discord.Client()
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION" ]});
 
+// Wachtkamer join log
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelID;
+    let oldUserChannel = oldMember.channelID;
+    const User = client.users.cache.get(`${newMember.id}`); 
+    const wachtKamerjoin = new Discord.MessageEmbed()
+    .setColor('#008d43')
+    .setTitle('LaagLandRP™ | Wachtkamer')
+    .setDescription(`${User} is de wachtkamer **gejoined!**`);
+    const wachtKamerleave = new Discord.MessageEmbed()
+    .setColor('#008d43')
+    .setTitle('LaagLandRP™ | Wachtkamer')
+    .setDescription(`${User} is de wachtkamer **geleaved**!`);
+
+    if(newUserChannel === "750249528036294706") //don't remove ""
+    { 
+        // User Joins a voice channel
+        client.channels.cache.get(`665937319445921798`).send(wachtKamerjoin)
+    }
+    else{
+        // User leaves a voice channel
+        client.channels.cache.get(`665937319445921798`).send(wachtKamerleave)
+    }
+ });
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -29,4 +54,4 @@ client.user.setActivity('youtube.com/GwnDaan', { type: 'WATCHING' })
     })
 })
 
-client.login(process.env.token)
+client.login(config.token);
